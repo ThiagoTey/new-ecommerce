fc1b9d272598f72cbd31d6a4902eba5e28f784c7
Thiago <teyextreme@outlook.com>
2025-03-04 16:19:46 -0300

    Atualização Link na logo e ProductCard

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { ProductType } from "@/types/ProductType";
 import ProductImage from "./ProductImage";
+import Link from "next/link";
 
 type Props = {
   product: ProductType;
@@ -7,14 +8,16 @@ type Props = {
 
 const ProductCard = ({ product }: Props) => {
   return (
-    <div className="cursor-pointer rounded-2xl border-[1px] border-transparent hover:border-gray-700 p-2">
-      <div className="relative w-[300px] h-[300px]">
-        <ProductImage product={product} fill />
-      </div>
-      <div className="max-w-[300px] max-h-[300px] flex flex-col gap-2 mt-2">
-        <p className="font-semibold text-xl">{product.title}</p>
-        <p className="font-bold text-2xl">R$ {product.price}</p>
-      </div>
+    <div className="cursor-pointer rounded-2xl border-[1px] border-transparent hover:border-gray-700 p-2 transition-all">
+      <Link href={`/product/${product.id}`}>
+        <div className="relative w-[300px] h-[300px]">
+          <ProductImage product={product} fill />
+        </div>
+        <div className="max-w-[300px] max-h-[300px] flex flex-col gap-2 mt-2">
+          <p className="font-semibold text-xl">{product.title}</p>
+          <p className="font-bold text-2xl">R$ {product.price}</p>
+        </div>
+      </Link>
     </div>
   );
 };
--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Logo = () => {
   return (
-    <h1 className="text-3xl font-bold">SHOPP.ING</h1>
+    <Link href={"/"}>
+      <h1 className="text-3xl font-bold">SHOPP.ING</h1>
+    </Link>
   )
 }
 
