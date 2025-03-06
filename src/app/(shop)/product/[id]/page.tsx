b22b31b4b956202ce365aa32a5fb0d6b0758f966
Thiago <teyextreme@outlook.com>
2025-03-06 10:23:23 -0300

    Fetch fakeApi in product id page

--- src/app/(shop)/product/[id]/page.tsx
+++ src/app/(shop)/product/[id]/page.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
 
-const Product = () => {
+async function getProduct(productId : string) {
+  const apiLink = `https://fakestoreapi.com/products/${productId}`
+  const res = await fetch(apiLink);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const Product = async ({params} : {params: { id: string };}) => {
+  const product = await getProduct(params.id);
+  console.log(product);
   return (
     <div>Product</div>
   )
